5667cf4da1b8426d07b3d530f367b2fcb2347866
vp49k1k145s <vp49k1k145s@mail.ru>
2019-06-17 20:38:27 +0300

    Дз 6

---
+++ .idea/src/lesson6/Dog.java
@@ -0,0 +1,7 @@
+package lesson6;
+
+public class Dog extends Animal {
+    public Dog(int runMaxDistance, int swimMaxDistance, double jumpMaxHeight) {
+        super(runMaxDistance, swimMaxDistance, jumpMaxHeight);
+    }
+}
