e41d7c826aa99dbc8ea7c0afc2a41d4a5b49946b
vp49k1k145s <vp49k1k145s@mail.ru>
2019-06-22 14:34:53 +0300

    Дз 7

--- .idea/src/lesson7/J1Lesson7.java
+++ .idea/src/lesson7/J1Lesson7.java
@@ -9,15 +9,16 @@ class J1Lesson7 {
         System.out.println("List of cats:");
         Cat[] x = {new Cat("Ben4", 7), new Cat("Ben5", 5), new Cat("Ben", 15), new Cat("Ben1", 15), new Cat("Ben2", 50), new Cat("Ben3", 100), new Cat("Ben6", 50)};
         Plate plate = new Plate(100);
-
-        for (Cat c : x) {
-            if (!c.isFull() && c.getAppetite() > plate.getFood()) {
-                plate.increaseFood(100);
-            }
-            c.eat(plate);
-            System.out.println(c);
-            System.out.println(plate);
+while (true) {
+    for (Cat c : x) {
+        if (!c.isFull() && c.getAppetite() > plate.getFood()) {
+            plate.increaseFood(100);
         }
+        c.eat(plate);
+        System.out.println(c);
+        System.out.println(plate);
+    }
+}
     }
 }
 
