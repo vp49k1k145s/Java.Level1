a3a85f3652499507e586c1c3749f04e18f875247
vp49k1k145s <vp49k1k145s@mail.ru>
2019-06-23 19:55:37 +0300

    дз8

--- .idea/src/Lesson8.java
+++ .idea/src/Lesson8.java
@@ -2,7 +2,7 @@
  * Java. Level 1. Lesson 8
  *
  * @author Dzyubenko Vadim
- * @version dated 20.06.2018
+ * @version dated 23.06.2018
  * profiles - https://github.com/vp49k1k145s
  * sertificat - https://geekbrains.ru/certificates/605078
  */
